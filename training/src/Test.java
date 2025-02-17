154338943086616cedc96cd0d38a5cdc4e3cfef5
gireesha-chandadi <gireeshachandadi87@gmail.com>
2025-02-17 19:49:43 +0530

    Init

---
+++ training/src/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+    public static void main(String[] args) {
+        //if &else statements
+        int marks=26;
+        if (marks>35) {
+            System.out.println("Congratulations u r passe in the exam");
+        }else{
+            System.out.println("you are failed in the exam better luck next time");
+        }
+
+
+    }
+}
